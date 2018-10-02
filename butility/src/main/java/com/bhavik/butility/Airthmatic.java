0aca66410bb4c5a36ca8b5cf1340051f204849d4
bhavik-bagdai <bhavikbagdai@gmail.com>
2018-10-02 13:44:00 +0530

    FirstTestCommit

---
+++ butility/src/main/java/com/bhavik/butility/Airthmatic.java
@@ -0,0 +1,35 @@
+package com.bhavik.butility;
+
+public class Airthmatic {
+    public double sum(double a,double b){
+        return  a+b;
+    }
+    public int sum(int a,int b){
+        return  a+b;
+    }
+    public String sum(String a,String b){
+        return  a+b;
+    }
+
+    public double min(double a,double b){
+        return  a-b;
+    }
+    public int min(int a,int b){
+        return  a-b;
+    }
+
+    public double mul(double a,double b){
+        return  a*b;
+    }
+    public int mul(int a,int b){
+        return  a*b;
+    }
+
+    public double div(double a,double b){
+        return  a/b;
+    }
+    public int div(int a,int b){
+        return  a/b;
+    }
+
+}
